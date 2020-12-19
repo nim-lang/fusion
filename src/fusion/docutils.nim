a5fcb635c02406cbcb438fc031343998592f2b62
Juan Carlos <JuanCarlospaco@gmail.com>
2020-12-19 04:27:07 -0300

    moar fixes

--- src/fusion/docutils.nim
+++ src/fusion/docutils.nim
@@ -1,18 +1,25 @@
-import std/[os,strformat,sugar,osproc]
+import std/[os, strformat, sugar, osproc]
 import std/private/globs
+
 
 iterator findNimSrcFiles*(dir: string): string =
   proc follow(a: PathEntry): bool =
-    a.path.lastPathPart notin ["nimcache", "htmldocs", "js"]
+    let dir = a.path.lastPathPart
+    if dir in  ["nimcache", "htmldocs"]: return false
+    when not defined(js): return true
+    else: return dir != "js"
+
   for entry in walkDirRecFilter(dir, follow = follow):
     if entry.path.splitFile.ext == ".nim" and entry.kind == pcFile:
       yield entry.path
+
 
 proc genCodeImportAll*(dir: string): string =
   result = "{.warning[UnusedImport]: off.}\n"
   for a in findNimSrcFiles(dir):
     let s = "".dup(addQuoted(a))
     result.add &"import {s}\n"
+
 
 proc genDocs(dir: string, nim = "", args: seq[string]) =
   let code = genCodeImportAll(dir)
@@ -21,6 +28,7 @@ proc genDocs(dir: string, nim = "", args: seq[string]) =
   let ret = execCmdEx(fmt"{nim} doc -r --project --docroot --outdir:htmldocs {extra} -", input = code)
   if ret.exitCode != 0:
     doAssert false, ret.output & "\n" & code
+
 
 when isMainModule:
   let args = commandLineParams()
