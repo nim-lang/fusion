7bde43d9ba03e08e9933daeb60706eb20df2b808
Juan Carlos <JuanCarlospaco@gmail.com>
2020-12-21 06:52:47 -0300

    Use  to prevent Tipos

--- src/fusion/js/jsheaders.nim
+++ src/fusion/js/jsheaders.nim
@@ -7,13 +7,13 @@ type Headers* = ref object of JsRoot ## HTTP Headers for the JavaScript target.
 func newHeaders*(): Headers {.importjs: "new Headers()".}
   ## https://developer.mozilla.org/en-US/docs/Web/API/Headers
 
-func append*(this: Headers; key: cstring; value: cstring) {.importjs: "#.$1(#, #)".}
+func add*(this: Headers; key: cstring; value: cstring) {.importjs: "#.append(#, #)".}
   ## https://developer.mozilla.org/en-US/docs/Web/API/Headers/append
 
 func delete*(this: Headers; key: cstring) {.importjs: "#.$1(#)".}
   ## https://developer.mozilla.org/en-US/docs/Web/API/Headers/delete
 
-func has*(this: Headers; key: cstring): bool {.importjs: "#.$1(#)".}
+func hasKey*(this: Headers; key: cstring): bool {.importjs: "#.has(#)".}
   ## https://developer.mozilla.org/en-US/docs/Web/API/Headers/has
 
 func keys*(this: Headers): seq[cstring] {.importjs: "Array.from(#.$1())".}
@@ -39,8 +39,8 @@ func clear*(this: Headers) {.importjs:
 runnableExamples:
   if defined(fusionJsHeadersTests):
     let header = newHeaders()
-    header.append(r"key", r"value")
-    doAssert header.has(r"key")
+    header.add(r"key", r"value")
+    doAssert header.hasKey(r"key")
     doAssert header.keys() == @["key".cstring]
     doAssert header.values() == @["value".cstring]
     doAssert header[r"key"] == "value".cstring
