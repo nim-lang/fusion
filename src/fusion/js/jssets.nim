d44734e5f2594398b715ed9401a53151cbd32e2b
Juan Carlos <JuanCarlospaco@gmail.com>
2020-12-25 11:50:36 -0300

    Add jssets

--- src/fusion/js/jssets.nim
+++ src/fusion/js/jssets.nim
@@ -1,57 +1,57 @@
 ## `Set` for the JavaScript target.
 ## * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
 when not defined(js) and not defined(nimdoc):
-  {.fatal: "Module jsset is designed to be used with the JavaScript backend.".}
+  {.fatal: "Module jssets is designed to be used with the JavaScript backend.".}
 
-type JSSet* = ref object of JsRoot ## Set API.
+type JsSet* = ref object of JsRoot ## Set API.
 
-func newJSSet*(): JSSet {.importjs: "new Set()".} ## Constructor for `JSSet`.
+func newJsSet*(): JsSet {.importjs: "new Set()".} ## Constructor for `JsSet`.
 
-func newJSSet*(items: openArray[SomeNumber]): JSSet {.importjs: "new Set(#)".} ## Constructor for `JSSet`.
+func newJsSet*(items: openArray[SomeNumber]): JsSet {.importjs: "new Set(#)".} ## Constructor for `JsSet`.
 
-func add*(this: JSSet; value: SomeNumber) {.importjs: "#.$1(#)".}
+func add*(this: JsSet; value: SomeNumber) {.importjs: "#.$1(#)".}
   ## https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/add
 
-func delete*(this: JSSet; value: SomeNumber) {.importjs: "#.$1(#)".}
+func delete*(this: JsSet; value: SomeNumber) {.importjs: "#.$1(#)".}
   ## https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/delete
 
-func clear*(this: JSSet) {.importjs: "#.$1()".}
+func clear*(this: JsSet) {.importjs: "#.$1()".}
   ## https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/clear
 
-func contains*(this: JSSet; value: SomeNumber): bool {.importjs: "#.has(#)".}
+func contains*(this: JsSet; value: SomeNumber): bool {.importjs: "#.has(#)".}
   ## https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/has
 
-func getInt*(this: JSSet): seq[int] {.importjs: """
+func getInt*(this: JsSet): seq[int] {.importjs: """
   (() => {const x = []; #.forEach(a => x.push(parseInt(a))); return x})()""".}
-  ## Convert `JSSet` to `seq[int]`, all items will be converted to `int`.
+  ## Convert `JsSet` to `seq[int]`, all items will be converted to `int`.
 
-func getFloat*(this: JSSet): seq[float] {.importjs: """
+func getFloat*(this: JsSet): seq[float] {.importjs: """
   (() => {const x = []; #.forEach(a => x.push(parseFloat(a))); return x})()""".}
-  ## Convert `JSSet` to `seq[float]`, all items will be converted to `float`.
+  ## Convert `JsSet` to `seq[float]`, all items will be converted to `float`.
 
 
 runnableExamples:
   if defined(fusionJsSetTests):
     block:
-      let a: JSSet = newJSSet([1, 2, 3, 4, 5])
-      let b: JSSet = newJSSet([1.0, 2.0, 3.0])
+      let a: JsSet = newJsSet([1, 2, 3, 4, 5])
+      let b: JsSet = newJsSet([1.0, 2.0, 3.0])
       doAssert a.getInt() == @[1, 2, 3, 4, 5]
       doAssert b.getFloat() == @[1.0, 2.0, 3.0]
       a.clear()
       b.clear()
       doAssert a.getInt() == @[]
       doAssert b.getFloat() == @[]
-      let d: JSSet = newJSSet([1, 2, 3])
+      let d: JsSet = newJsSet([1, 2, 3])
       d.add(4)
       d.delete(2)
       doAssert 3 in d
       doAssert d.contains 1
 
     block:
-      let a = newJSSet([1, 2])
+      let a = newJsSet([1, 2])
       a.add(3.5)
       doAssert a.getInt() == @[1, 2, 3]
 
-      let b = newJSSet([1.0, 2.0])
+      let b = newJsSet([1.0, 2.0])
       b.add(3)
       doAssert b.getFloat() == @[1.0, 2.0, 3.0]
