fece46c0aff0ea2a5ba01aa6931ee4a3a450df5f
Juan Carlos <JuanCarlospaco@gmail.com>
2020-12-30 21:43:48 -0300

    remove export

--- src/fusion/js/jsxmlserializer.nim
+++ src/fusion/js/jsxmlserializer.nim
@@ -3,7 +3,6 @@ when not defined(js) and not defined(nimdoc):
   {.fatal: "Module jsxmlserializer is designed to be used with the JavaScript backend.".}
 
 from dom import Node
-export Node
 
 type XMLSerializer* = ref object of JsRoot  ## XMLSerializer API.
 
