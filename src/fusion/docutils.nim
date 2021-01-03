553c1db634fc0eeb173d41f5b5902b1988d991e7
Juan Carlos <JuanCarlospaco@gmail.com>
2021-01-03 16:51:54 -0300

    more fix

--- src/fusion/docutils.nim
+++ src/fusion/docutils.nim
@@ -1,10 +1,10 @@
-import std/[os, sugar, osproc]
+import std/[os, sugar, strutils, osproc]
 import std/private/globs
 
 
 const
   blockList = ["nimcache", "htmldocs"] # Folders to explicitly ignore.
-  docComand = " doc -r --project --docroot --outdir:htmldocs --styleCheck:hint -" # nim doc command part that never changes
+  docComand = " doc --project --docroot --outdir:htmldocs --styleCheck:hint " # nim doc command part that never changes
 
 
 iterator findNimSrcFiles*(dir: string): string =
@@ -22,7 +22,7 @@ proc genCodeImportAll*(dir: string): string =
   for nimfile in findNimSrcFiles(dir):
     name = nimfile.extractFilename
     prefix =
-      if name[0] == 'j' and name[1] == 's':
+      if name.startsWith "js":
         "when defined(js): import "
       else:
         "when not defined(js): import "
@@ -33,7 +33,7 @@ proc genDocs(dir: string, nim = "", args: seq[string]) =
   let code = genCodeImportAll(dir)
   let extra = quoteShellCommand(args)
   let nim = if nim.len == 0: getCurrentCompilerExe() else: nim
-  let ret = execCmdEx(nim & ' ' & extra & docComand, input = code)
+  let ret = execCmdEx(nim & docComand & extra & " - ", input = code)
   if ret.exitCode != 0:
     doAssert false, ret.output & '\n' & code
 
