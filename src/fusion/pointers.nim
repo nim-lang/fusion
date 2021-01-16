0e596a269526a9b16c0e15a2a12ba4af522a4622
Timothee Cour <timothee.cour2@gmail.com>
2021-01-15 21:34:57 -0800

    add addrCast

--- src/fusion/pointers.nim
+++ src/fusion/pointers.nim
@@ -16,10 +16,31 @@ proc toUncheckedArray*[T](a: ptr T): ptr UncheckedArray[T] {.inline.} =
     doAssert a[1] == 105
   cast[ptr UncheckedArray[T]](a)
 
-proc `cast`*[T](a:T, T2: typedesc): T2 =
-  ## allows running `cast` in UFCS/method call syntax chains.
+template `cast`*[T](a:T, T2: typedesc): untyped =
+  ## Same as `cast[T2](a)` but can be used in UFCS/MCS chains.
   ## Unsafe as it calls `cast`.
   runnableExamples:
-    proc square(a: auto): auto = a*a
-    doAssert 3.square.cast(float).square == cast[float](3.square).square
+    var a = [1'u8, 2, 3, 4]
+    # the MCS chain reads left to right:
+    let x1 = a.cast(array[2, uint16])[0].cast(float32)
+    # instead of a zig-zag:
+    let x2 = cast[float32](cast[array[2, uint16]](a)[0])
+    doAssert x1 == x2
   cast[T2](a)
+
+template addrCast*[T](a:T, T2: typedesc): untyped =
+  ## Same as `cast[ptr T2](unsafeAddr(a))[]` but can be used in UFCS/MCS chains.
+  ## Unsafe as it calls `cast`.
+  runnableExamples:
+    proc fn(a: var auto): auto =
+      a[0].inc
+      a
+    var a = [1'u8, 2, 3, 4]
+    let b = fn(a.addrCast(array[2, uint16]))
+    doAssert a == [2'u8, 2, 3, 4]
+    when system.cpuEndian == littleEndian:
+      doAssert b == [514'u16, 1027]
+      doAssert a.addrCast(uint32) == 67305986'u32
+      a.addrCast(uint32).inc
+      doAssert a == [3'u8, 2, 3, 4]
+  cast[ptr T2](unsafeAddr(a))[]
